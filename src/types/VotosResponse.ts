c6c1d615994a304cb4c8acadb86b6bf8def664cb
arck <prueba.arck@gmail.com>
2024-05-15 15:54:56 +0200

    commit

---
+++ src/types/VotosResponse.ts
@@ -0,0 +1,8 @@
+export type VotosResponse {
+    id:             number;
+    nombre:         string;
+    bandera:        string;
+    cancionYoutube: string;
+    cantante:       string;
+    cancion:        string;
+}
