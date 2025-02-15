b7c19e6a40fa381413d7b31f798bd97f350828ae
arck <prueba.arck+github@gmail.com>
2025-02-15 21:01:11 +0100

    cambio a tabla y arreglo de endpoints

---
+++ src/types/Resultados.ts
@@ -0,0 +1,9 @@
+export type Resultados = {
+  id:             number;
+  nombre:         string;
+  bandera:        string;
+  cancionYoutube: string;
+  cantante:       string;
+  cancion:        string;
+  votoTotal:      number;
+}
